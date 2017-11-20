f8e0b2709a1967c6f2a8653c0542958b6a6228a2
Daniel Rejon <rejonpardenilla@codn.mx>
2017-11-20 10:23:22 -0600

    add example on file changed

--- main/src/AppMain.java
+++ main/src/AppMain.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.util.concurrent.TimeUnit;
 
 public class AppMain {
 
@@ -8,29 +9,32 @@ public class AppMain {
         String filePath = new File("").getAbsolutePath();
         String configurationFilePath = filePath + "/main/src/config.json";
 
-        try {
-            DatabaseConnectionsPool pool = new DatabaseConnectionsPool(configurationFilePath);
-            DatabaseConnection connection = pool.acquireConnection();
+        while (true) {
+            try {
+                DatabaseConnectionsPool pool = new DatabaseConnectionsPool(configurationFilePath);
+                DatabaseConnection connection = pool.acquireConnection();
 
-            ResultSet rs = connection.getConnection().query("SELECT 1+1 as Suma FROM DUAL;");
-            ResultSetMetaData rsmd = rs.getMetaData();
-            int columnsNumber = rsmd.getColumnCount();
-            System.out.println( "Column|Value");
+                ResultSet rs = connection.getConnection().query("SELECT * FROM duck;");
+                ResultSetMetaData rsmd = rs.getMetaData();
+                int columnsNumber = rsmd.getColumnCount();
+                System.out.println( "Column|Value");
 
 
-            while (rs.next()) {
-                for (int i = 1; i <= columnsNumber; i++) {
-                    if (i > 1) System.out.print(",  ");
-                    String columnValue = rs.getString(i);
-                    System.out.print( rsmd.getColumnName(i) + "|" + columnValue);
+                while (rs.next()) {
+                    for (int i = 1; i <= columnsNumber; i++) {
+                        if (i > 1) System.out.print(",  ");
+                        String columnValue = rs.getString(i);
+                        System.out.print( rsmd.getColumnName(i) + "|" + columnValue);
+                    }
+                    System.out.println("");
                 }
-                System.out.println("");
+
+                pool.releaseConnection(connection);
+                TimeUnit.SECONDS.sleep(10);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
-            pool.releaseConnection(connection);
-        } catch (Throwable e) {
-            e.printStackTrace();
+            System.out.println("--------------");
         }
-
     }
 }
